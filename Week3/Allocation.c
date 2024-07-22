b12576cf9cc7748db153ba451097e68050cafbd1
NangManCat123 <nangmancat123@gmail.com>
2024-07-23 05:53:49 +0900

    Upload Allocation.c

---
+++ Week3/Allocation.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	int num, dist;
+	int* array;
+	scanf("%d", &num);
+	array = calloc(num, sizeof(int));
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &array[i]);
+	}
+	scanf("%d", &dist);
+	array = (int*)realloc(array, (num + dist) * sizeof(int));
+	for (int i = num; i < num + dist; i++) {
+		scanf("%d", &array[i]);
+	}
+	for (int i = 0; i < num + dist; i++) {
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
