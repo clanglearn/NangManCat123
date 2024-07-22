2ee1d386e2826ab4986a8c38e5498b862e31ddaf
NangManCat123 <nangmancat123@gmail.com>
2024-07-23 03:43:23 +0900

    Upload Malloc.c

---
+++ Week3/Malloc.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	int size;
+	scanf("%d", &size);
+	int* array;
+	array = (int*)malloc(size * sizeof(int));
+	for (int i = 0; i < size; i++) {
+		scanf("%d", &array[i]);
+	}
+	for (int i = 0; i < size; i++) {
+		printf("%d ", array[i]);
+	}
+	return 0;
+}
